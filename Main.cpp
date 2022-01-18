2f28d1a225f8661f4b81b54120e2f49a6ec44552
eulseon <95990910+es915@users.noreply.github.com>
2022-01-18 11:41:24 +0900

    const function 2

--- Main.cpp
+++ Main.cpp
@@ -1,65 +1,13 @@
 #include <iostream>
-#include "Main.h"
-#include "Person.h"
-#include "DataStore.h"
+#include "MyMath.h"
 
-using namespace std;
-
-class Test {
-private : 
-	int data;
-
-public :
-	Test() :
-		data(10)
-	{
-
-	}
-
-	// const 멤버 함수
-	void printData() const {
-		cout << data << endl;
-	}
-	
-	// const 멤버 함수 - 멤버 변수를 변경할수 없다
-	void plusData(int addData) const {
-		// data = data + addData; // 변경 불가
-		cout << data + addData << endl;
-	}
-};
+// 아래는 MyMath 클래스를 이용해 빼기와 나누기 값을 출력해주는 함수입니다.
+// 멤버변수의 잘못된 변경으로 에러가 나고 있습니다. 멤버변수를 바꾸지 않고 출력만 할 수 있도록 강제해주세요.
 
 int main() {
 
-	Person p1{ 20, "홍길동" };
-	p1.introduce();
+	MyMath math;
 
-	Person p2;
-
-	test();
-
-	DataStore ds;
-
-	DataStore* ds2 = new DataStore();
-
-	ds.setData(1);
-	ds.setData(2);
-	ds.setData(3);
-
-	ds2->setData(1);
-	ds2->setData(2);
-	ds2->setData(3);
-
-	int rst = ds.getData(1);
-	int rst2 = ds2->getData(2);
-
-	cout << rst << endl;
-	cout << rst2 << endl;
-
-	delete ds2;
-
-	return 0;
-}
-
-void test() {
-	cout << "test" << endl;
+	math.Minus(10);
+	math.Divide(100);
 }
---
+++ MyMath.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class MyMath {
+public:
+	MyMath();
+	void Divide(int num) const;
+	void Minus(int num) const;
+private:
+	int mX;
+};
